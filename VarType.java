516ae140e1d230ba8171193264b797495db9a94c
howie-student <howie@chouforce.com>
2025-08-20 17:08:19 +0000

    8 var types

---
+++ VarType.java
@@ -0,0 +1,41 @@
+public class VarType { 
+
+    public static void main(String[] args) { 
+
+        byte b = 100; 
+
+        short s = 1000; 
+
+        int i = 100000; 
+
+        long l = 10000000000L; 
+
+        float f = 3.14f; 
+
+        double d = 3.14159265359; 
+
+        char c = 'A'; 
+
+        boolean bool = true; 
+
+  
+
+        System.out.println("byte: " + b); 
+
+        System.out.println("short: " + s); 
+
+        System.out.println("int: " + i); 
+
+        System.out.println("long: " + l); 
+
+        System.out.println("float: " + f); 
+
+        System.out.println("double: " + d); 
+
+        System.out.println("char: " + c); 
+
+        System.out.println("boolean: " + bool); 
+
+    } 
+
+} 
